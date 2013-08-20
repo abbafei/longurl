dc6468f704cf62bf3b2cd6fd7dacff2c84339d7e
yossi <yossi@yy-pc>
2013-08-20 03:13:33 -0400

    B"H add test URLs info in case it changes

--- test_urls.sh
+++ test_urls.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 for flags in '-f 0' '-a' '-f 0 -a' '-f 0 -a -p'; do
 	printf 'Flags "%s"\n' "$flags"
+	# plus.google.com uses relative URL redirects currently.
+	# opengroup URL redirects to empty string, which is a relative url meaning current url, therefore would loop forever
+	# gmail is a regular URL which redirects a few times
+	# fhwfw... is an invalid one since it uses invalid characters
 	for url in 'plus.google.com/u/0/110933955439424623638/posts' 'www.opengroup.org/bookstore/catalog/c082.htm' 'gmail.com' 'fhwfw???.invaliddomainnamethisisone4testing'; do
 		printf '\tURL "%s"\n' "$url"
 		if [ "$flags" = '-f 0' ]; then
