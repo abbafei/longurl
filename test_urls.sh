20e62692fbc97bcb829f8e4199505c71db5f697a
yossi <yossi@yy-pc>
2013-08-20 05:23:07 -0400

    B"H google plus url
    change the google plus url from a random gplus.to url from twitter
    search.

--- test_urls.sh
+++ test_urls.sh
@@ -5,7 +5,7 @@ for flags in '-f 0' '-a' '-f 0 -a' '-f 0 -a -p'; do
 	# opengroup URL redirects to empty string, which is a relative url meaning current url, therefore would loop forever
 	# gmail is a regular URL which redirects a few times
 	# fhwfw... is an invalid one since it uses invalid characters
-	for url in 'plus.google.com/u/0/110933955439424623638/posts' 'www.opengroup.org/bookstore/catalog/c082.htm' 'gmail.com' 'fhwfw???.invaliddomainnamethisisone4testing'; do
+	for url in 'plus.google.com/u/0/116899029375914044550/posts' 'www.opengroup.org/bookstore/catalog/c082.htm' 'gmail.com' 'fhwfw???.invaliddomainnamethisisone4testing'; do
 		printf '\tURL "%s"\n' "$url"
 		if [ "$flags" = '-f 0' ]; then
 			(longurl "$url" $flags 2>&1 >/dev/null); printf '\t\tExit code %s\n' "$?"
